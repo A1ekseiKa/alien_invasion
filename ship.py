e793cf7ec5ab9c06615b019e37d2c3b1f5a8fd8e
Aleksei_Ka <aleksei.kozel@gmail.com>
2022-08-06 13:32:54 +0300

    Добавлена регулировка скорости перемещения корабля

--- ship.py
+++ ship.py
@@ -7,6 +7,7 @@ class Ship():
     def __init__(self, ai_game):
         """Инициализирует корабль и задает его начальную позицию."""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
         # Загружает изображение корабля и получает прямоугольник.
         self.image = pygame.image.load('images/ship.bmp')
@@ -14,16 +15,20 @@ class Ship():
         # Каждый новый корабль появляется у нижнего края
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Сохранение вещественной координаты центра корабля.
+        self.x = float(self.rect.x)
+
         # Флаг перемещения
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
         """Обновляет позицию корабля с учетом флагов."""
+        # Обновляется атрибут x, а не rect.
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
 
     def blitme(self):
         """Рисует корабль в текущей позиции."""
